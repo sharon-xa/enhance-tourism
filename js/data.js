1b2bc6a316f232ede9025248c76849896b24a048
sharon <ali93456@gmail.com>
2024-12-11 03:44:57 +0300

    add the places section with cards

---
+++ js/data.js
@@ -0,0 +1,8 @@
+export const places = [
+	{
+		name: "Ruins of the North Palace of king Nebuchadnezzar II",
+		image: "../assets/Ruins_of_the_North_Palace_of_king_Nebuchadnezzar_II_at_the_ancient_city_of_Babylon_Mesopotamia_Iraq.jpg",
+		location: "Babil",
+		rating: "4.8",
+	},
+];
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+import { places } from "./data.js";
+
 // The DOMContentLoaded event fires when the HTML document has been completely parsed, and all deferred scripts (<script defer src="…"> and <script type="module">) have downloaded and executed. It doesn't wait for other things like images, subframes, and async scripts to finish loading.
 document.addEventListener("DOMContentLoaded", () => {
 	const main = document.querySelector("main");
+	const navLinks = document.querySelectorAll("nav a");
 
 	// Function to load content dynamically
 	const loadContent = async (page) => {
@@ -14,6 +17,10 @@ document.addEventListener("DOMContentLoaded", () => {
 			main.innerHTML = content;
 			document.title =
 				page.charAt(0).toUpperCase() + page.slice(1) + " - My App";
+
+			// Update the active link
+			highlightActiveLink(page, navLinks);
+			if (page.toLowerCase() === "home") loadCards();
 		} catch (error) {
 			main.innerHTML = "<h1>404 - Page Not Found</h1>";
 			console.error(error);
@@ -38,6 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 
 	// Load the initial page
+	let initialPage;
 	if (location.pathname === "/" || location.pathname === "/index.html") {
 		initialPage = "home";
 	} else {
@@ -48,3 +56,56 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	loadContent(initialPage);
 });
+
+// Function to highlight the active link in the navbar
+const highlightActiveLink = (page, navLinks) => {
+	navLinks.forEach((link) => {
+		if (link.getAttribute("data-page") === page) {
+			link.classList.add("pc-text");
+		} else {
+			link.classList.remove("pc-text");
+		}
+	});
+};
+
+// Function to load the cards based on the data in the data.js file
+const loadCards = () => {
+	const cardsContainer = document.querySelector(".cards");
+
+	places.forEach((place) => {
+		const newA = document.createElement("a");
+		newA.innerHTML = `
+				<div class="card">
+					<img
+						src="${place.image}"
+						alt="${place.name}"
+					/>
+					<p class="name">${place.name}</p>
+					<div class="details-cont">
+						<hr
+							style="
+								height: 1px;
+								border: none;
+								color: #333;
+								background-color: #ccc;
+							"
+						/>
+						<div class="details">
+							<div class="location">
+								<img src="../assets/location-dot-solid.svg" />
+								<span>${place.location}</span>
+							</div>
+
+							<div class="rating">
+								<img src="../assets/star-solid.svg" />
+								<span>${place.rating}</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			`;
+		newA.href = place.name;
+
+		cardsContainer.appendChild(newA);
+	});
+};
