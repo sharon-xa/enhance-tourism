e4056f6b48c5c4ebeb48ec5a43b861840acc86a5
sharon <ali93456@gmail.com>
2025-02-03 07:08:51 +0300

    not finished

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			body.insertAdjacentHTML("beforeend", footer);
 
 			setActiveLink();
-			setupSearch();
 		} catch (error) {
 			console.error(error);
 			body.innerHTML = "<h1>Failed to load header and footer</h1>";
@@ -46,21 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
 				link.classList.remove("active");
 			}
 		});
-	};
-
-	const setupSearch = () => {
-		document
-			.querySelector(".search-form")
-			.addEventListener("submit", function (event) {
-				event.preventDefault();
-
-				const query = document.getElementById("search").value.trim();
-				if (query) {
-					window.location.href = `/pages/search.html?q=${encodeURIComponent(
-						query
-					)}`;
-				}
-			});
 	};
 
 	loadHeaderAndFooter();
